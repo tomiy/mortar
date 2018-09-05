2375ade7476133be558da2c39763d7d86f749dbb
Tom Chappaz <tom.chappaz1@gmail.com>
2018-09-05 17:05:54 +0200

    Added 404 callback and some doc

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ use Foundation\Tools\Debug;
 Router::get('/', function() {
 	echo 'hello world';
 });
-Router::get('/int:key/str:test?/', function($key, $test = 'lol') {
+
+Router::get('/int:key/str:test?/', function($key, $test = 'default') {
 	echo "$key/$test";
 });
