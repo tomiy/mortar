eafff247a4876adb03f3adcc358fe0950800b561
Tom Chappaz <tom.chappaz1@gmail.com>
2018-10-22 15:47:58 +0200

    triggering codacy again

--- app/views/compiled/7a2a165d31da7fce6dd77e05c300028a.php
+++ app/views/compiled/7a2a165d31da7fce6dd77e05c300028a.php
@@ -1,11 +1,6 @@
 <?php#1538788231?>
-
 <input type="hidden" name="_token" value="<?= hash_hmac('sha256', CURRENT_URI, $_SESSION['csrf_token']); ?>"/>
-
 <input type="hidden" name="_token" value="<?= hash_hmac('sha256', CURRENT_URI, $_SESSION['csrf_token']); ?>"/>
-
 <input type="hidden" name="_token" value="<?= hash_hmac('sha256', CURRENT_URI, $_SESSION['csrf_token']); ?>"/>
-
 <input type="hidden" name="_token" value="<?= hash_hmac('sha256', CURRENT_URI, $_SESSION['csrf_token']); ?>"/>
-
 <input type="hidden" name="_token" value="<?= hash_hmac('sha256', CURRENT_URI, $_SESSION['csrf_token']); ?>"/>
