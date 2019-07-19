fd2f4cf857be2fd94dc28195c4ebffb7e3c98b3f
tomiy <tom.chappaz1@gmail.com>
2019-07-19 15:55:46 +0200

    temporarily add the core to the controller (need a way to push a view object to dependant parties or sth)

--- engine/display/controller.class.php
+++ engine/display/controller.class.php
@@ -2,12 +2,16 @@
 
 namespace Mortar\Engine\Display;
 
+use Mortar\Foundation\Tools\DependencyInjector as DI;
+
 class Controller
 {
     protected $request;
+    protected $mortar;
 
     public function __construct($request)
     {
         $this->request = $request;
+        $this->mortar = DI::get('core');
     }
 }
